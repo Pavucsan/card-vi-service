f6db479b0bf44f64e697e8b66a8c0550f2462fb2
“pavucsan.manokaran” <smpavucsan@gmail.com>
2022-12-18 13:43:26 +0530

    code refactor

--- src/main/java/com/virtusa/cardservice/CardServiceApplication.java
+++ src/main/java/com/virtusa/cardservice/CardServiceApplication.java
@@ -2,12 +2,13 @@ package com.virtusa.cardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableDiscoveryClient
+//@EnableResourceServer
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CardServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CardServiceApplication.class, args);
